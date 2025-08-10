eb83d12d2cb2d51ea1450ce7e521ba640e77e828
yfdd617235 <yosefdavid91@gmail.com>
2025-08-09 20:50:38 -0500

    tasks get

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,19 +1,45 @@
-import {Auth} from './Auth'
+import { Auth } from './Auth'
 import { useState, useEffect } from 'react'
 import { db } from '../config/firebase'
+import { getDocs, collection } from 'firebase/firestore'
 
 export const Tasks = () => {
-const [tasksList, setTasksList] = useState([])
+    const [tasksList, setTasksList] = useState([]);
+    const tasksCollectionRef = collection(db, "Tasks");
 
-useEffect(() => {
-    const getTasksList = async () => {
-//Read Data
-//Set the movie List https://www.youtube.com/watch?v=2hR-uWjBAgw 38:41
-    }
-})
+    useEffect(() => {
+        const getTasksList = async () => {
+            //Read Data
+            //Set the movie https://www.youtube.com/watch?v=2hR-uWjBAgw 47:57
+            try {
+                const data = await getDocs(tasksCollectionRef);
+                const filteredData = data.docs.map((doc) => ({
+                    ...doc.data(),
+                    id: doc.id,
+                }));
+                // console.log(filteredData);
+                setTasksList(filteredData)
+            } catch (err) {
+                console.error(err)
+            }
 
-    return(
-        <div> tasks Here</div>
+        }
+        getTasksList();
+    }, []);
+
+    return (
+        <div>
+
+            <div> tasks Here</div>
+            <div>{tasksList.map((task) => (
+                <div key={task.id}> 
+                    <h1> Customer: {task.Customer} {task.id}</h1>
+                    <p>Status: {task.Status}</p>
+                    <p>Amount: {task.Amount}</p>
+                </div>
+            ))}</div>
+        </div>
+
     )
 }
 
